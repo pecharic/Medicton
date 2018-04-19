ab9010febed984cfed75d5370356810b8d96b870
Richard Pecha <pecharic@fit.cvut.cz>
2018-04-19 12:15:46 +0200

    add browserouter

--- medicton/src/App.tsx
+++ medicton/src/App.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
-
+import {  Route } from 'react-router-dom';
+import {Home} from './pages/index';
 
 export default  class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        ahoh
+            <Route exact path="/" component={Home}/>
+           <Route path="/admin" component={Admin}/>
+           <Route path="/dashboard" component={User}/>
       </div>
     );
   }
 }
 
+
+const Admin = () => {
+  return (
+     <div>admin</div>
+  )
+}
+
+const User = () => {
+  return (
+      <div>User</div>
+  );
+}
---
+++ medicton/src/pages/home/home.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export class Home extends React.Component<{}, {}>{
+
+    render() {
+        return (<div>home</div>)
+        ;}
+}
