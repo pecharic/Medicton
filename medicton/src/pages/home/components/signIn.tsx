2ee78b4f69defd446241d8985a00ede799c8d6c2
Richard Pecha <pecharic@fit.cvut.cz>
2018-04-20 11:32:51 +0200

    bug1132

--- medicton/src/pages/home/components/signIn.tsx
+++ medicton/src/pages/home/components/signIn.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
-interface IProps{
-    changeVisible:string;
-}
-export class SignIn extends React.Component<IProps, {}>{
-  
+
+export class SignIn extends React.Component<{}, {}>{
+    constructor(props:any){
+        super(props);
+    }
     render() {
         return (
             <div>
-                <h2>LOG IN{this.props.changeVisible}</h2>
+              
                 <TextField
                         hintText="E-mail"
                         floatingLabelText="E-mail"
--- medicton/src/pages/home/home.tsx
+++ medicton/src/pages/home/home.tsx
@@ -1,23 +1,16 @@
 import * as React from 'react';
 import { SignIn } from './components/index';
 
-interface IState{
-    visible:boolean
-}
 
-export class Home extends React.Component<{}, IState>{
-    constructor(props:any){
-        super(props);
-        this.state={
-            visible:true
-        };
-    }
+
+export class Home extends React.Component<{}, {}>{
+   
     render() {
         return (
             <div>
                 <h1>home</h1>
                 <br />
-                <SignIn changeVisible="ahoj"/>
+                <SignIn/>
             </div>
         )
             ;
